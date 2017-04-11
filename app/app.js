5e50bbd1f35a1b37b1f147b9de5c26a7b79dbf42
John Frier <jfrier@gmail.com>
2017-04-10 18:06:34 -0700

    moving partials into app directory

--- app/app.js
+++ app/app.js
@@ -3,13 +3,13 @@ var module = angular.module("TheaterShoppingCart", ["ngRoute"]);
 module.config(function($routeProvider) {
     $routeProvider
         .when("/", {
-            templateUrl: "../partials/home.html"
+            templateUrl: "partials/home.html"
         })
         .when("/shoppingcart", {
-          templateUrl: "../partials/shoppingcart.html",
+          templateUrl: "partials/shoppingcart.html",
           controller: "ShoppingCartCtl"
         })
         .when("/admin", {
-            templateUrl: "../partials/admin.html"
+            templateUrl: "partials/admin.html"
         });
 });
